1533af648a2b935498e6480a99f70f9519745a95
latin1gor <latinigor.main@gmail.com>
2024-07-11 13:29:44 +0300

    feat: hello page

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import {View, Text, TouchableOpacity, ActivityIndicator, FlatList} from 'react-native'
 
 import styles from './popularjobs.style'
+import {useRouter} from "expo-router";
+import {COLORS, SIZES} from "../../../constants";
+import PopularJobCard from "../../common/cards/popular/PopularJobCard";
 
 const Popularjobs = () => {
+    const router = useRouter()
+    const isLoading = false
+    const error = false
   return (
-    <View>
-      <Text>Popularjobs</Text>
+    <View style={styles.container}>
+        <View style={styles.header}>
+            <Text style={styles.headerTitle}>Popular jobs</Text>
+            <TouchableOpacity><Text style={styles.headerBtn}>Show all</Text></TouchableOpacity>
+        </View>
+        <View style={styles.cardsContainer}>
+            {isLoading ? <ActivityIndicator size={"large"} color={COLORS.primary} /> : error ? <Text>Something went wrong</Text> : <FlatList data={[0,1,2,3,4,5]} renderItem={({item}) => (
+                <PopularJobCard
+                    item={item}
+               />
+            )}  keyExtractor={(item) => item?.job_id} contentContainerStyle={{columnGap: SIZES.medium}} horizontal/>}
+
+        </View>
     </View>
   )
 }
