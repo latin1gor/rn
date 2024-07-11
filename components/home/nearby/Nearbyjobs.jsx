51c0af280591a901513572d77fbe91a39aeeb551
latin1gor <latinigor.main@gmail.com>
2024-07-11 15:12:09 +0300

    feat: useFetch hook

--- components/home/nearby/Nearbyjobs.jsx
+++ components/home/nearby/Nearbyjobs.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { View, Text } from 'react-native'
-
-import styles from './nearbyjobs.style'
+import {useFetch} from "../../../hooks/useFetch";
 
 const Nearbyjobs = () => {
+    const a = useFetch("1")
+    console.log(a)
   return (
     <View>
       <Text>Nearbyjobs</Text>
---
+++ hooks/useFetch.js
@@ -0,0 +1,50 @@
+import {useEffect, useState} from "react";
+import { RAPID_API_KEY} from "@env"
+import axios from "axios";
+const rapidApiKey = RAPID_API_KEY
+export const useFetch = (endpoint, query) => {
+    const [data, setData] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
+    const [error, setError] = useState(null)
+
+
+
+    const options = {
+        method: 'GET',
+        url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+        params: {
+           ...query
+        },
+        headers: {
+            'x-rapidapi-key': rapidApiKey,
+            'x-rapidapi-host': 'jsearch.p.rapidapi.com'
+        }
+    };
+
+    const fetchData = async () => {
+        setIsLoading(true)
+
+        try {
+            const res = await axios.request(options)
+            setData(res.data.data
+                setIsLoading(false)
+
+        } catch (e){
+            setError(e)
+            alert("There is an error", e)
+        }
+        finally {
+setIsLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, []);
+
+    const refetch = () => {
+        setIsLoading(true)
+        fetchData()
+    }
+    return {data, isLoading, error, refetch}
+}
