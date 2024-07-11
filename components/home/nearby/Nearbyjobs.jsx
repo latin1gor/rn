c5b8d9708facfa3402943d930b2689b42e3de5c7
latin1gor <latinigor.main@gmail.com>
2024-07-11 15:18:16 +0300

    fix nearbyjobs

--- components/home/nearby/Nearbyjobs.jsx
+++ components/home/nearby/Nearbyjobs.jsx
@@ -1,10 +1,7 @@
 import React from 'react'
 import { View, Text } from 'react-native'
-import {useFetch} from "../../../hooks/useFetch";
 
 const Nearbyjobs = () => {
-    const a = useFetch("1")
-    console.log(a)
   return (
     <View>
       <Text>Nearbyjobs</Text>
