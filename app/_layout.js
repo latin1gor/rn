3a1ece9db084b24d0f9e93dd07d5aeebc5ad2c23
latin1gor <latinigor.main@gmail.com>
2024-07-11 10:10:07 +0300

    initial commit

---
+++ app/_layout.js
@@ -0,0 +1,9 @@
+import {SplashScreen, Stack} from "expo-router";
+
+const Layout = () => {
+
+
+    return <Stack  />
+}
+
+export default Layout
---
+++ app/index.js
@@ -0,0 +1,38 @@
+import { View, ScrollView, SafeAreaView, Text} from "react-native";
+import {useState} from "react";
+import {Stack, useRouter} from "expo-router";
+import { COLORS, icons, images, SIZES} from "../constants";
+import { Nearbyjobs, Popularjobs, ScreenHeaderBtn } from '../components'
+import Welcome from "../components/home/welcome/Welcome";
+const Home = () => {
+    const router = useRouter()
+    const [state, setState] = useState()
+    return (
+        <SafeAreaView style={{flex: 1, backgroundColor: COLORS.lightWhite}}>
+            <Stack.Screen options={{headerStyle: {backgroundColor: COLORS.lightWhite},
+            headerShadowVisible: false,
+                headerLeft: () => (
+                    <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+                ),
+                headerRight: () => (
+                    <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />
+                ),
+                headerTitle: ""
+            }} />
+
+            <ScrollView showsVerticalScrollIndicator={false}>
+               <View style={{flex: 1,
+               padding: SIZES.medium}}>
+
+                   <Welcome />
+                   <Popularjobs/>
+                   <Nearbyjobs />
+
+               </View>
+            </ScrollView>
+
+        </SafeAreaView>
+    )
+}
+
+export default Home
