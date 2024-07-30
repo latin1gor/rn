029f33436159ad40f6086345fa2033cf8436ce72
latin1gor <latinigor.main@gmail.com>
2024-07-30 15:46:40 +0300

    feat: responsibilities

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -32,6 +32,7 @@ const JobDetails = () => {
           case "About":
               return <JobAbout info={data[0].job_description || "No data provided"} />
           case "Responsibilities":
+              return <Respo title={"Responsibilities"} points={data[0].job_highlights?.Responsibilities ?? ["N/A"]}/>
           default:
               break
       }
